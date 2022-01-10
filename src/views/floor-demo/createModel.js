0c7c3f4f142c54990a2a541f43214f9bd09e63c4
chenshuideshizi <fanwenxiang326@163.com>
2022-01-10 22:22:20 +0800

    feature:修改demo

--- src/views/floor-demo/createModel.js
+++ src/views/floor-demo/createModel.js
@@ -4,57 +4,51 @@ export default {
     data(){
         return{
             _borderData: [], // 楼房基础数据
-            _bounds: [ // 楼房四角
-                [116.396168,39.997583], // left-top
-                [116.397003,39.997604], // right-top
-                [116.397066,39.996032], // bottom-right
-                [116.396217,39.996001] // bottom-left
-            ], 
-            _center: [116.396622,39.996875] // 楼房中心点
+            _bounds: [], // 楼房四至
+            _center: [] // 楼房中心点
         }
     },
-    created() {
-
-    },
     methods: {
+
         // 创建场景
         initContainer(borderData) {
-            // 创建场景
+            /*创建场景对象Scene*/
             var scene = new THREE.Scene()
-
             // 添加楼房楼层平面
             this.addFloors(scene, borderData)
-
+            /* 光源设置*/
             // 环境光
             var ambientLight = new THREE.AmbientLight(0xffffff)
             scene.add(ambientLight)
-
-            // 创建相机对象
+            /**
+             * 相机设置
+             */
+            var width = window.innerWidth //窗口宽度
+            var height = window.innerHeight //窗口高度
+            var k = width / height //窗口宽高比
+            var s = 1500 //三维场景显示范围控制系数，系数越大，显示的范围越大
+            //创建相机对象
             var camera = new THREE.PerspectiveCamera(
                 45,
                 window.innerWidth / window.innerHeight,
                 0.1,
                 1000000
             )
-            camera.position.set(300,200,300)
+            camera.position.set(3000,2000,3000)
             camera.lookAt(scene.position)
-
-            // 添加格子辅助线
-            let grid = new THREE.GridHelper( 400, 30, 0xcccccc, 0xcccccc );
-            scene.add( grid );
-
-            // 创建渲染器对象
+            /**
+             * 创建渲染器对象
+             */
             var renderer = new THREE.WebGLRenderer({
                 antialias: true,
                 alpha: true
             })
-            renderer.setSize(window.innerWidth, window.innerHeight)
+            renderer.setSize(window.innerWidth - 300, window.innerHeight)
             document.getElementById('modelBox').appendChild(renderer.domElement)
 
-            // 控制器
             var controls = new OrbitControls(camera, renderer.domElement)
             controls.target = new THREE.Vector3(0, 0, 0) //控制焦点
-            controls.autoRotate = false;//将自动旋转关闭
+            controls.autoRotate = true;//将自动旋转关闭
             let clock = new THREE.Clock();//用于更新轨道控制器
 
             function animate() {
@@ -69,34 +63,25 @@ export default {
 
         // 添加楼群
         addFloors(scene, data) {
+            // 数据处理/基础数据计算
+            this._dataAnalysis(data)
+            this._getModelBounds()
+            this._getModelCenter()
+            this._dataAnalysisAll()
             // 新建楼房组
             var group = new THREE.Group();
             group.rotation.set(-1.6,0,0);
             scene.add(group)
             // 添加楼层
-            this._borderData.features.forEach(feature => {
-                this.addFloor(group,feature)
+            this._borderData.forEach(res => {
+                this.addFloor(group,res)
                 // 添加楼房黄色边框墙
-                this.createWall(group, feature)
+                this.createWall(group, res)
             })
         },
         // 添加单个楼
-        addFloor (group, feature) {
-            const coordinates = feature.geometry.coordinates
-            const floor = feature.properties.Floor
-            const points = coordinates.map(r => {
-                // 将度转换为米
-                r = r.map(re => {
-                    return [(re[0]*1112000).toFixed(0)*1, (re[1]*1112000).toFixed(0)*1]
-                })
-
-                return ({
-                    floor: Floor,
-                    points: r
-                })
-            })
-
-            let shape = this.createShape(points)
+        addFloor (group, data) {
+            let shape = this.createShape(data.points)
             let i = 0
             let addG = setInterval(() => {
                 if (i<data.floor) {
@@ -119,11 +104,65 @@ export default {
                 }
             }, 30)
         },
+        // 数据转换
+        _dataAnalysis (borderData) {
+            let data = []
+            borderData.features.forEach(res => {
+                res.geometry.coordinates.forEach(r => {
+                        // 将度转换为米
+                    r = r.map(re => {
+                        return [(re[0]*1112000).toFixed(0)*1, (re[1]*1112000).toFixed(0)*1]
+                    })
+                    data.push({
+                        floor: res.properties.Floor,
+                        points: r
+                    })
+                })
+            })
+            this._borderData = data
+        },
+        // 获取模型四至
+        _getModelBounds () {
+            // 四至： 上右下左
+            let bounds = [0,0,0,0]
+            // 所有点数组
+            let pointArr = []
+            // 所有点经度数组
+            let pointLonArr = []
+            // 所有点纬度数组
+            let pointLatArr = []
+            // 获取所有点数组
+            this._borderData.forEach(res => {
+                pointArr = pointArr.concat(res.points)
+            })
+            // 获取经度、纬度数组
+            pointArr.forEach(res => {
+                pointLonArr.push(res[0])
+                pointLatArr.push(res[1])
+            })
+            // 获取四至
+            bounds = [Math.max(...pointLatArr), Math.min(...pointLonArr), Math.min(...pointLatArr), Math.max(...pointLonArr)]
+            this._bounds = bounds
+        },
+        // 获取模型中心点
+        _getModelCenter () {
+            let center = [(this._bounds[1] + this._bounds[3])/2,(this._bounds[0] + this._bounds[2])/2]
+            this._center = center
+        },
+        // 数据转换2-将数据移动至原点
+        _dataAnalysisAll(){
+            this._borderData.forEach(res => {
+                res.points.forEach(re => {
+                    re[0] = re[0] - this._center[0]
+                    re[1] = re[1] - this._center[1]
+                })
+            })
+        },
 
         // 创建平面集合
-        createShape (points) {
+        createShape (data) {
             var shape = new THREE.Shape();
-            points.forEach((e,i) => {
+            data.forEach((e,i) => {
                 if (i === 0) {
                     shape.moveTo( ...e);
                 } else {
