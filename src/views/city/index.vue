bf26b63f6e82a329bef318e5f3ab2c2502a9c465
fanwenxiang <fanwenxiang@aiforward.com>
2022-01-13 20:08:35 +0800

    feature:修改写法

--- src/views/city/index.vue
+++ src/views/city/index.vue
@@ -83,15 +83,14 @@ export default {
     onDocumentMouseDown(event) {
         event.preventDefault();
         const {camera, scene} = this
-        var vector = new THREE.Vector3();//三维坐标对象
-        vector.set(
-            (event.clientX / window.innerWidth ) * 2 - 1,
-            - (event.clientY / window.innerHeight ) * 2 + 1,
-            0.5
-        );
-        vector.unproject( camera );
-        var raycaster = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
-        var intersects = raycaster.intersectObjects(scene.children);
+        const raycaster = new THREE.Raycaster();
+        const mouse = new THREE.Vector2();
+
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+        raycaster.setFromCamera( mouse, camera );
+        const intersects = raycaster.intersectObjects( scene.children );
+
         if (intersects.length > 0) {
             var selected = intersects[0];//取第一个物体
             console.log('selected', selected)
